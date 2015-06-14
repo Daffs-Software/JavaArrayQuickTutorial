96eac6c98ec55e1ff20ee1c656521f85022b0fe7
Matthew <bkc4life@hotmail.com>
2015-06-14 17:47:13 +0800

    Create Example2.java

---
+++ Example2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class Example2
+{
+	public static void main(String[] args)
+	{
+		int[] intExample2 = {1,2,3,4,5};						//Declare the Array
+			
+		System.out.println(intExample2[0]);		//print out the value
+		System.out.println(intExample2[1]);		//print out the value		
+		System.out.println(intExample2[2]);		//print out the value
+		System.out.println(intExample2[3]);		//print out the value
+		System.out.println(intExample2[4]);		//print out the value
+		
+		System.out.println(intExample2[5]);		//Out of bounds exception
+		
+		System.exit(0);
+	}
+}
