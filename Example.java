fa6cd62a8740ead54b8576e187e8a1bea6601bd5
Matthew <bkc4life@hotmail.com>
2015-06-14 17:46:51 +0800

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Example
+{
+	public static void main(String[] args)
+	{
+		int[] intExample;						//Declare the Array
+		
+		intExample = new int[10];				//Initialize the Array
+		
+		intExample[0] = 1;						//Set the value
+		
+		System.out.println(intExample[0]);		//print out the value
+		
+		
+		System.exit(0);
+	}
+}
