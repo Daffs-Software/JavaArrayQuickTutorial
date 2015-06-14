63d4942cfa6d2744c17a09688ce8a41664515e74
Matthew <bkc4life@hotmail.com>
2015-06-14 17:47:34 +0800

    Update Example2.java

--- Example2.java
+++ Example2.java
@@ -11,7 +11,7 @@ public class Example2
 		System.out.println(intExample2[3]);		//print out the value
 		System.out.println(intExample2[4]);		//print out the value
 		
-		System.out.println(intExample2[5]);		//Out of bounds exception
+		//System.out.println(intExample2[5]);		//Out of bounds exception
 		
 		System.exit(0);
 	}
